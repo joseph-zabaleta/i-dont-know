f1d791e56b1b941f2b0b7aea91d95471a8d095d2
Iris Leal <iris_leal_m@hotmail.com>
2020-06-09 21:09:40 -0700

    Displaying order list on terminal

--- i_dont_care/history.py
+++ i_dont_care/history.py
@@ -2,6 +2,19 @@ import json
 from datetime import date
 
 
+def display_orders_history():
+    order_list = get_history_list(True)
+
+    if len(order_list) == 0:
+        print('There are not orders saved yet....')
+        return
+    
+    print('These are the orders so far...\n\n')
+    for order in order_list:
+        print(order + "\n")
+
+
+
 def get_history_list(reversed = None):
     """
     This method returns all the orders in a list, where each order is in one position on the array.
@@ -10,13 +23,11 @@ def get_history_list(reversed = None):
     try:
         order_list = []
         with open('./assets/orders_history.txt') as json_file:
-            history = json.load(json_file)
-        
+            history = json.load(json_file)        
 
         for order in history["orders"]:
-            order_info =  order['user'] + " ordered " + order['order'] + " on " + order['date']
-            order_list.append(order_info)
-        
+            order_info = "On " + order['date'] + ", " + order['user'] + " ordered " + order['order']
+            order_list.append(order_info)        
         
         if reversed : order_list.reverse()
         
@@ -57,22 +68,23 @@ def add_order_to_history(user, food):
     with open('./assets/orders_history.txt', 'w') as outfile:
       json.dump(order_history, outfile)
 
+def load_dummy_data():
+    add_order_to_history('John', 'Indian')
+    add_order_to_history('Erich', 'Tai')
+    add_order_to_history('Gabriela', 'Mexican')
+    add_order_to_history('Marie', 'Italian')
+    add_order_to_history('Jonh', 'Hamburguers')
+    add_order_to_history('Erich', 'Carne Asada')
+    add_order_to_history('Erich', 'Hotcakes')
+    add_order_to_history('Marie', 'Tacos')
+
 
 
 if __name__ == "__main__":
-    # add_order_to_history('John', 'Indian')
-    # add_order_to_history('Erich', 'Tai')
-    # add_order_to_history('Gabriela', 'Mexican')
-    # add_order_to_history('Marie', 'Italian')
-    # add_order_to_history('Jonh', 'Hamburguers')
-    # add_order_to_history('Erich', 'Carne Asada')
-    # add_order_to_history('Erich', 'Hotcakes')
-    # add_order_to_history('Marie', 'Tacos')
-
-
-
-    print(get_history_list())
-    print("\n *********")
-    print(get_history_list(True))
+    # load_dummy_data()
+    # print(get_history_list())
+    # print("\n *********")
+    # print(get_history_list(True))
+    display_orders_history()
 
 
