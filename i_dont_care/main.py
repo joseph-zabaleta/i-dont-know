b8f77f91f00216ea7bc418e8bd534f84739a9afd
Iris Leal <iris_leal_m@hotmail.com>
2020-06-10 08:00:22 -0700

    First working draft of adding the user to the history

--- i_dont_care/main.py
+++ i_dont_care/main.py
@@ -3,18 +3,31 @@ from i_dont_care import (
     intro,
     food_tree,
     scraper,
+    history
     # show_results,
     # exit
 )
 
+def promp_user():
+    user = ""
+    # all_users =  history.get_users()
+
+    while user == "":
+        user = input('Please enter the user who is going to choose today... ')
+
+    return(user)
 
 if __name__ == "__main__":
 
-    if curses.wrapper(intro.main):
-        search_query = (food_tree.start_app())  # make sure there is a return at the end of one value
-
-        results = scraper.scrape_yelp(search_query)  # searches yelp and gets/returns results
+    # if curses.wrapper(intro.main): # TODO: IRIS, un comment this
+    user = promp_user()
+    # print(user)
+    search_query = (food_tree.start_app())  # make sure there is a return at the end of one value
+    results = scraper.scrape_yelp(search_query)  # searches yelp and gets/returns results
 
         # show_results.show() which renders all the results of the search
+    # print('the food to order is ', search_query)
 
+    history.add_order_to_history(user, search_query)
+    # history.display_orders_history()
     # exit.goodbye() which runs the thank you message / closes program
