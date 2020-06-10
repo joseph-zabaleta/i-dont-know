163208f0e8d25e0436fde84b760a418e85666e3b
Iris Leal <iris_leal_m@hotmail.com>
2020-06-09 20:40:25 -0700

    Now the get_history_list method can display the orders list in order desc or asc

--- i_dont_care/history.py
+++ i_dont_care/history.py
@@ -2,9 +2,10 @@ import json
 from datetime import date
 
 
-def get_history_list():
+def get_history_list(reversed = None):
     """
     This method returns all the orders in a list, where each order is in one position on the array.
+    And returns in in the indicated order
     """
     try:
         order_list = []
@@ -15,6 +16,9 @@ def get_history_list():
         for order in history["orders"]:
             order_info =  order['user'] + " ordered " + order['order'] + " on " + order['date']
             order_list.append(order_info)
+        
+        
+        if reversed : order_list.reverse()
         
         return order_list
     except:
@@ -60,10 +64,15 @@ if __name__ == "__main__":
     # add_order_to_history('Erich', 'Tai')
     # add_order_to_history('Gabriela', 'Mexican')
     # add_order_to_history('Marie', 'Italian')
-    # add_order_to_history('Jonh', 'Hamburguer')
-    # orders = get_history()
+    # add_order_to_history('Jonh', 'Hamburguers')
+    # add_order_to_history('Erich', 'Carne Asada')
+    # add_order_to_history('Erich', 'Hotcakes')
+    # add_order_to_history('Marie', 'Tacos')
 
-    # print(orders["orders"][0]['user'])
+
+
     print(get_history_list())
+    print("\n *********")
+    print(get_history_list(True))
 
 
