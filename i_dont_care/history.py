3031b1d3650ab068b7d2cff0aba40fc6661fd848
Iris Leal <iris_leal_m@hotmail.com>
2020-06-09 20:10:40 -0700

    Now it returns the hostory information in a list

--- i_dont_care/history.py
+++ i_dont_care/history.py
@@ -2,6 +2,25 @@ import json
 from datetime import date
 
 
+def get_history_list():
+    """
+    This method returns all the orders in a list, where each order is in one position on the array.
+    """
+    try:
+        order_list = []
+        with open('./assets/orders_history.txt') as json_file:
+            history = json.load(json_file)
+        
+
+        for order in history["orders"]:
+            order_info =  order['user'] + " ordered " + order['order'] + " on " + order['date']
+            order_list.append(order_info)
+        
+        return order_list
+    except:
+        return []
+
+
 def get_history():
     try:
       with open('./assets/orders_history.txt') as json_file:
@@ -38,10 +57,13 @@ def add_order_to_history(user, food):
 
 if __name__ == "__main__":
     # add_order_to_history('John', 'Indian')
-    add_order_to_history('Erich', 'Tai')
-    add_order_to_history('Gabriela', 'Mexican')
-    add_order_to_history('Marie', 'Italian')
-    add_order_to_history('Jonh', 'Hamburguer')
-    
+    # add_order_to_history('Erich', 'Tai')
+    # add_order_to_history('Gabriela', 'Mexican')
+    # add_order_to_history('Marie', 'Italian')
+    # add_order_to_history('Jonh', 'Hamburguer')
+    # orders = get_history()
+
+    # print(orders["orders"][0]['user'])
+    print(get_history_list())
 
 
